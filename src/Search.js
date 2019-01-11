cc6f87cf98bd75140c70a95de07d3303e0ee12e9
csenge <csengeweb@gmail.com>
2019-01-11 11:53:12 +0100

    added controlled component - BUG

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,32 @@
 import React from 'react'
-//import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import {Link} from 'react-router-dom'
 import Book from './Book'
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          query: '',
+          searchedBookList: [] 
+        
+        }
+      }
+    
+    updateQuery = (query) => {
+        this.setState({ query: query.trim() })
+        this.searchBooks(query)
+    }  
+
+    searchBooks = (query) => {
+        query? (BooksAPI.search(query)
+                .then((searchedBookList)=> {
+                    this.setState({searchedBookList:searchedBookList}) 
+                })
+            ) 
+            : this.setState({searchedBookList: []})
+    }
     render(){
         return(
             <div className="search-books">
@@ -14,19 +36,32 @@ class Search extends React.Component {
               </Link>
               <div className="search-books-input-wrapper">
                 {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
+                  NOTES: The BooksAPI.search method DOES search by title or author. The search from BooksAPI is limited to a particular set of search terms.
+                  Find search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input 
+                    type="text" 
+                    placeholder="Search by title or author" 
+                    value={this.state.query} 
+                    onChange = {(event)=> this.updateQuery(event.target.value)} />
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+              {this.state.searchedBookList.map(singleBook => {
+                  return (<li  key={singleBook.id}>
+                    <Book 
+                        key={singleBook.id}
+                        singleBook = {singleBook}
+                        currentShelf = {singleBook.shelf}
+                        changeShelf = {this.props.changeShelf}/>
+                  </li>)
+              })
+            }
+                
+              </ol>
             </div>
           </div>
         )
@@ -34,3 +69,12 @@ class Search extends React.Component {
 }
     
 export default Search
+
+/* TODO:
+1. in state : BooksApi.search() books
+2. get books with BooksApi.search()
+3. solve when search input is empty
+4. add controlled component
+5. shelfChanger should work
+6. default currentShelf:'none' EXCEPT MyBooks books
+*/
